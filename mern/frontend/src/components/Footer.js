c325ef6727432445e69f4102b8c4a3594de318d2
Manmohan Sahoo <manmohansahoo361@gmail.com>
2021-03-27 20:37:32 +0530

    changes

--- mern/frontend/src/components/Footer.js
+++ mern/frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; EasyShop</Col>
+          <Col className='text-center py-3'>Copyright &copy; EasyBuy</Col>
         </Row>
       </Container>
     </footer>
